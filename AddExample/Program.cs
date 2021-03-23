96f58c3c0a42901ce1dbd07ad9b6d0160647a473
Ryan Andersen <redninja106@live.com>
2021-03-23 18:26:01 -0400

    Add AddExample example project

---
+++ AddExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Commander;
+using System;
+
+namespace AddExample
+{
+    class Program
+    {
+        [Command]
+        [Doc("Adds two numbers together")]
+        [DocArg("int", "a", "The first number to add")]
+        [DocArg("int", "b", "The second number to add")]
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Main(string[] args)
+        {
+            // print the docs of add
+            Service.SubmitCommandString("doc add");
+
+            // add 40 and 60
+            Service.SubmitCommandString("add 40 60");
+
+            Console.ReadLine();
+        }
+    }
+}
