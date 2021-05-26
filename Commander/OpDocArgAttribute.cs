ffe273ccdd4b34b6eea0f48d3a25f77b97fa282e
Ryan Andersen <redninja106@live.com>
2021-05-26 15:22:42 -0400

    Add Description for OpDocArgAttribute class.

--- Commander/OpDocArgAttribute.cs
+++ Commander/OpDocArgAttribute.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace Commander
 {
+    /// <summary>
+    /// Command documentation attribute for optional command arguments.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
     public sealed class OpDocArgAttribute : Attribute
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="OpDocArgAttribute"/> class.
+        /// </summary>
+        /// <param name="type">The type of value of the argument.</param>
+        /// <param name="name">The name of the of the argument.</param>
+        /// <param name="defaultValue">The default value of the arugment.</param>
+        /// <param name="doc">The description of the argument.</param>
         public OpDocArgAttribute(string type, string name, string defaultValue, string doc)
         {
             Doc = doc;
